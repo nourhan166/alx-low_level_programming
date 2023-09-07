d49979e43f6ce92da5b9bec61a70b599d791a14e
nourhan166 <nourhanmazed@gmail.com>
2023-09-07 09:13:07 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main -Entry point
+ * main - Entry point
  *
  * Description: A C program that prints with put function
  *
@@ -10,6 +10,6 @@
 
 int main(void)
 {
-   puts("\"Programming is like building a multilingual puzzle"):
-   return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
