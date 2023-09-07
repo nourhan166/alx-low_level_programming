17da4a264d3e8ca5a1ab9c6ae8ab3101fef0427b
nourhan166 <nourhanmazed@gmail.com>
2023-09-07 09:52:33 -0700

    n

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,6 @@
 #include <Stdio.h>
-
 /**
- * main - Entry point
+ *main - Entry point
  *
  * Description: A C progam that prints with printf function.
  *
@@ -9,7 +8,7 @@
 */
 
 int main(void)
-{ 
+{
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
