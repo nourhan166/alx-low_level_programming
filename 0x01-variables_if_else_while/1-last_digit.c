fc9210070307da53dff37627614d32e972f86a65
nourhan166 <nourhanmazed@gmail.com>
2023-09-08 16:41:29 -0700

    mm

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -5,8 +5,8 @@
 /**
  * main - entry point
  *
- * Description: print the value of n status:
- * greater than, is zero and is not less than 6.
+ * Description: print the value of n status greater
+ * than is zero and is not less than 6
  *
  * Return: 0 (success)
 */
