8727248d61aee57670d35d84e15d36d999f0844d
nourhan166 <nourhanmazed@gmail.com>
2023-09-18 14:18:56 -0700

    mm

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,22 +8,11 @@
  */
 void puts2(char *str)
 {
-int longi = 0;
-int t = 0;
-char *y = str;
-int o;
-while (*y != '\0')
+int i;
+for (i = 0; str[i] != '\0'; ++i)
 {
-y++
-longi++
-}
-t = longi - 1;
-for (o = 0 ; o <= t ; o++)
-{
-if (o % 2 == 0)
-{
-_putchar(str[o]);
-}
+if (i % 2 == 0)
+_putchar(str[i]);
 }
 _putchar('\n');
 }
