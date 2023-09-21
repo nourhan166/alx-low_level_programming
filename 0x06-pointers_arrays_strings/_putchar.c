9c43f39271891adf9ab5611b121666ff87f45874
nourhan166 <nourhanmazed@gmail.com>
2023-09-21 16:07:49 -0700

    a

---
+++ 0x06-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,12 @@
+#include <unstd.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: the character to print
+ * Return: On success 1.
+ * on error, -1 is returned, and arrno is sat appropriately.
+ */
+int _putchar(char c)
+{
+return (write(1, &c, 1));
+}
