4a2d451f5a76340680432c62d2895c7cfe924116
nourhan166 <nourhanmazed@gmail.com>
2023-10-02 08:38:15 -0700

    BB

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -5,7 +5,7 @@
  * @argv:pointer to an array of command line arguments.
  * Return:0-sucess, non-zero-fail.
  */
-int main(int argc, char *argv[] __attribute__(unused))
+int main(int argc, char *argv[] __attribute__((unused)))
 {
 printf("%d\n", argc - 1);
 return (0);
