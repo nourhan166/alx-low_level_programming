e523af68ee0811055ba225dc98617bf412d58748
nourhan166 <nourhanmazed@gmail.com>
2023-10-10 14:40:39 -0700

    a

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * a dog struct
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: owner's name
+ * Description: a dog struct
+ */
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+#endif
