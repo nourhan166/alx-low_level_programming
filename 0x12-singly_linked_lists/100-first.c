30a83b88fb0bb5369f4ff7e277d894ecfc169985
nourhan166 <nourhanmazed@gmail.com>
2023-10-19 14:41:44 -0700

    m

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -7,5 +7,5 @@ void _constructor(void) __attribute__ ((constructor));
 void _constructor(void)
 {
 printf("you're beat! and yet, you must allow,\n"
-		"I bore my house upon my back!\n");
+"I bore my house upon my back!\n");
 }
