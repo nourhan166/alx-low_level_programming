58f5af1100d9afe8ccace6d11a3c950529b70679
nourhan166 <nourhanmazed@gmail.com>
2023-10-19 13:40:53 -0700

    BB

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,13 +1,14 @@
 #ifndef LISTS_H
 #define LISTS_H
+
 #include <stdio.h>
-#include <stdlio.h>
+#include <stdlib.h>
 #include <string.h>
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc 'ed string)
  * @len: length of the string
- * @next: points to the next
+ * @next: points to the next node
  * Description: singly linked list node structure
  * for holberton projects
  */
