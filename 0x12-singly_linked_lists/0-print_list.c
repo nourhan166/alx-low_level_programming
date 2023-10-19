d6df273277b890abe7843f4184fee1a2d80147a4
nourhan166 <nourhanmazed@gmail.com>
2023-10-19 13:49:59 -0700

    hh

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -14,7 +14,7 @@ i++;
 return (i);
 }
 /**
- * print_list -prints a linked lists
+ * print_list - prints a linked lists
  * @h: pointer to first node
  * Return: size of list
  */
@@ -24,7 +24,7 @@ size_t i = 0;
 while (h)
 {
 printf("[%d] %s\n", _strlen(h->str), h->str ? h->str : "(nil)");
-h = h->next
+h = h->next;
 i++;
 }
 return (i);
