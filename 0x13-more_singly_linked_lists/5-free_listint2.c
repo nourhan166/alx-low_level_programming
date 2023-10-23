c711367d603b1eeb4ae71526bd1d037c05b8009c
nourhan166 <nourhanmazed@gmail.com>
2023-10-23 13:12:41 -0700

    a

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 /**
- * free_listint2 - frees a lists of ints
- * @head: address of pointer to the first node
+ * free_listint2 - frees a list of ints
+ * @head: address of pointer to first node
  * Return: void
  */
 void free_listint2(listint_t **head)
