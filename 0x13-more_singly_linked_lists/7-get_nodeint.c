ddd5f39a0a0e04ab32686b1b2dde014ea94b0876
nourhan166 <nourhanmazed@gmail.com>
2023-10-23 13:28:43 -0700

    M

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+/**
+ * get_nodeint_at_index - returns node at index
+ * @head: pointer to the first node
+ * @index: index of nude to get
+ * Return: pointer to node
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+listint_t *node;
+unsigned int n;
+for (node = head, n = 0; node && n < index; node = node->next, n++)
+return (node);
+}
