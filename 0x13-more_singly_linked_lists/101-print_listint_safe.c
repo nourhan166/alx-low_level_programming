eaf3f2d02d8854a8b50c5ddc5355862b6add49ca
nourhan166 <nourhanmazed@gmail.com>
2023-10-24 09:04:42 -0700

    kkl

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -9,10 +9,11 @@
  * @new: new node to add to the list
  * Return: pointer to the new list
  */
-const listint_t **_r(const listint_t *new)
+const listint_t **_r(const listint_t **list, size_t size, const listint_t *new)
 {
 const listint_t **newlist;
 size_t i;
+
 newlist = malloc(size * sizeof(listint_t *));
 if (newlist == NULL)
 {
@@ -28,6 +29,7 @@ return (newlist);
 /**
  * print_listint_safe - prints a listint_t linked list.
  * @head: pointer to the start of the list
+ *
  * Return: the number of nodes in the list
  */
 size_t print_listint_safe(const listint_t *head)
