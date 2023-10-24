a1e6323bf44bd3678d03f234e6d2216e66b127df
nourhan166 <nourhanmazed@gmail.com>
2023-10-24 09:07:22 -0700

    B

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -5,8 +5,9 @@
  * _r - reallocates memory for an array of pointers
  * to the nodes in a linked list
  * @list: the old list to append
- * @size: size of the new list always one more than the old list
+ * @size: size of the new list (always one more than the old list)
  * @new: new node to add to the list
+ *
  * Return: pointer to the new list
  */
 const listint_t **_r(const listint_t **list, size_t size, const listint_t *new)
