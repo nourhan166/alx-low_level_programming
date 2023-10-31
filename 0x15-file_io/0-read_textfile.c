8512416c4e0ab8e69d300511449258acd9714c11
nourhan166 <nourhanmazed@gmail.com>
2023-10-31 14:36:31 -0700

    l

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,6 +1,7 @@
 #include "main.h"
+
 /**
- * read_textfile - reads text from the file and prints it
+ * read_textfile - reads text from a file and prints it
  * @filename: name of file to read
  * @letters: number of bytes to read
  * Return: number bytes read or printed
@@ -12,7 +13,7 @@ ssize_t bytes;
 char buf[READ_BUF_SIZE * 8];
 if (!filename || !letters)
 return (0);
-fd = open(filename, o_RDONL Y);
+fd = open(filename, o_RDONLY);
 if (fd == -1)
 return (0);
 bytes = read(fd, &buf[0], letters);
